a0c8dcf636af4b2718a89549a4481fa4d83ef1d9
Dusky <1042932843@qq.com>
2019-07-08 16:21:33 +0800

    设置页面的缓存管理和版本号显示

--- app/src/main/java/com/dusky/world/Module/activities/SetActivity.java
+++ app/src/main/java/com/dusky/world/Module/activities/SetActivity.java
@@ -13,11 +13,15 @@ package com.dusky.world.Module.activities;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.dusky.world.Base.BaseActivity;
+import com.dusky.world.Base.DuskyApp;
 import com.dusky.world.R;
+import com.dusky.world.Utils.CacheDataManager;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 
 /**
  * @ClassName: SetActivity
@@ -29,6 +33,24 @@ public class SetActivity extends BaseActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+
+    @BindView(R.id.ver)
+    TextView ver;
+
+    @BindView(R.id.size)
+    TextView size;
+
+    @OnClick(R.id.check_for_updates)
+    public void check(){
+
+    }
+
+    @OnClick(R.id.clean_up_cache)
+    public void clean(){
+        CacheDataManager.clearAllCache(SetActivity.this);
+        setCache();
+    }
+
 
     @Override
     public int getLayoutId() {
@@ -44,6 +66,19 @@ public class SetActivity extends BaseActivity {
             supportActionBar.setDisplayHomeAsUpEnabled(true);
         }
 
+        ver.setText(String.format("Ver.%s", DuskyApp.getInstance().getAppVersionName(this)));
+
+        setCache();
+    }
+
+    public void setCache(){
+        String cache="";
+        try {
+            cache=CacheDataManager.getTotalCacheSize(SetActivity.this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        size.setText(cache);
     }
 
     @Override
